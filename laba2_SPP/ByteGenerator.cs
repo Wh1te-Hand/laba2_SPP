4c3f99e3a802cddd8cf2bbc490f9721f2d21ab93
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2022-11-15 21:29:56 +0300

    Add Generator

---
+++ laba2_SPP/ByteGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_SPP
+{
+    public class ByteGenerator:IGenerator
+    {
+           
+        public object Generate()
+        {
+            var random = new Random();
+            byte result = 0;
+            while (result == 0)
+            {
+                result = (byte)random.Next(byte.MinValue, byte.MaxValue);
+            }
+            return result;
+        }
+
+        public Type getGeneratedType()
+        {
+            return typeof(byte);
+        }
+    
+}
+}
