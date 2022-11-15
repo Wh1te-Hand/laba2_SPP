c541a2f03d3d5e2531b13ef689d5f7b310edd06e
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2022-11-15 21:29:32 +0300

    Beginning

--- laba2_SPP/Program.cs
+++ laba2_SPP/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using laba2_SPP;
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+        Faker faker = new Faker();
+
+        Console.ReadLine();
+    }
+}
